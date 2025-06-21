a315d72bc868b3e961a867e57bf9ae9b31407405
skmdJeesan <skmdjeesan@gmail.com>
2025-06-21 12:36:12 +0530

    format

--- 6-unlimitedColors/chaiaurcode.js
+++ 6-unlimitedColors/chaiaurcode.js
@@ -4,28 +4,27 @@ const randomColor = function () {
     const hex = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
-      color += hex[Math.floor(Math.random() * 16)];
+        color += hex[Math.floor(Math.random() * 16)];
     }
     return color;
-  };
-  
-  let intervalId;
-  const startChangingColor = function () {
+};
+
+let intervalId;
+const startChangingColor = function () {
     if (!intervalId) {
-      intervalId = setInterval(changeBgColor, 1000);
+        intervalId = setInterval(changeBgColor, 1000);
     }
-  
+
     function changeBgColor() {
-      document.body.style.backgroundColor = randomColor();
+        document.body.style.backgroundColor = randomColor();
     }
-  };
-  const stopChangingColor = function () {
+};
+const stopChangingColor = function () {
     clearInterval(intervalId);
     intervalId = null;
-  };
-  
-  document.querySelector('#start').addEventListener('click', startChangingColor);
-  
-  document.querySelector('#stop').addEventListener('click', stopChangingColor);
-  
-  
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
+
