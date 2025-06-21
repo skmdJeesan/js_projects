6b38c081edc030d001048a2f2e4245dc324d7713
skmdJeesan <skmdjeesan@gmail.com>
2025-06-21 10:27:00 +0530

    hellow! commit changes

---
+++ 2-BMICalculator/chaiaurcode.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#results');
+
+    if(height <= 0 || height === '' || isNaN(height)){
+        result.innerHTML = `not a valid height`;
+    } else if(weight <= 0 || weight === '' || isNaN(weight)){
+        result.innerHTML = `not a valid weight`;
+    } else {
+        const bmi = (weight / (height/100)**2).toFixed(2);
+        if(bmi < 18.6) result.innerHTML = `your BMI is ${bmi}<br>you are under weight`;
+        else if(bmi >= 18.6 && mi <= 24.9) result.innerHTML = `your BMI is ${bmi}<br>you are in normal range`;
+        else result.innerHTML = `your BMI is ${bmi}<br>you are over weight`;
+    }
+})
